6aa2c8e9c1f5da57f40a55873aabfc8c5a80a216
Gaurav Kumar Thakur <gaurav.21scse1011042@galgotiasuniversity.edu.in>
2024-06-22 16:47:07 +0530

    Completed week3

---
+++ week3/fspromise.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs/promises');
+
+// fs.readFile("file.txt", "utf-8")
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
+
+// Using Async Await
+async function readFile() {
+    try{
+        const data = await fs.readFile("file.txt", "utf-8");
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+readFile();
